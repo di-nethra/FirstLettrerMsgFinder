b5894efa0dd5c447a91d42dcb24d76ef285a643c
di-nethra <anjana.dinethra@gmail.com>
2021-05-14 21:34:51 +0530

    initial comment

---
+++ app.py
@@ -0,0 +1,12 @@
+print("\n\n!!!SECRET MESSAGE DECRYPTER!!!!")
+
+input("Press Enter to continue...")
+myString=input("\nenter the secret message:")
+word=myString.split()
+first_letter=[word[0] for word in word]
+
+print("".join(first_letter))
+
+
+# try this message as a test
+# Sera alfred lampard Gemini apple hogwarts anatomy Lumberjack anjalo ginsen antimony Brisbane alphs longbottom lanscap eeternal kenya
